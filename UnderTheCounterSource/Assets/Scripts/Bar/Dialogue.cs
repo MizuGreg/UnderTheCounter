cfd5e054d9c03df86a84e7f0d12ab53a08f582c0
Gregory <gregorio.dimaglie@mail.polimi.it>
2024-11-12 12:18:49 +0100

    complete merge of dialogue system into dev

--- UnderTheCounterSource/Assets/Scripts/Bar/Dialogue.cs
+++ UnderTheCounterSource/Assets/Scripts/Bar/Dialogue.cs
@@ -1,7 +1,10 @@
-namespace Bar
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
 {
-    public class Dialogue
-    {
-        
-    }
+    public string name;
+
+    [TextArea(3, 10)]
+    public string[] sentences;
 }
--- UnderTheCounterSource/Assets/Scripts/Bar/DialogueManager.cs
+++ UnderTheCounterSource/Assets/Scripts/Bar/DialogueManager.cs
@@ -1,32 +1,62 @@
 using System.Collections;
+using System.Collections.Generic;   
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace Bar
-{
-    public class DialogueManager : MonoBehaviour
-    {
-        public CanvasGroup dialogueCanvas;
+public class DialogueManager : MonoBehaviour {
 
-        public void startDialogue(Dialogue dialogue)
-        {
-            
-        }
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
 
-        private void displayNextSentence()
-        {
-            
-        }
+    private Queue<string> sentences;
 
-        private IEnumerator typeSentence(string sentence)
-        {
-            return null;
-        }
-
-        private void endDialogue()
-        {
-            
-        }
-        
-    
+    // Initialization
+    void Start() {
+        sentences = new Queue<string>();
     }
+
+    public void StartDialogue(Dialogue dialogue) 
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) 
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() 
+    {
+
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) 
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray()) 
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue() 
+    {
+        animator.SetBool("IsOpen", false);
+    }
+
 }
