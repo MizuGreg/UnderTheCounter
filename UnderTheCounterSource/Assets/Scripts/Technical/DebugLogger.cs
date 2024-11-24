c582ee0a49ca45190b7bb54c8381a4060e1b7709
Gregory <gregorio.dimaglie@mail.polimi.it>
2024-11-24 19:52:43 +0100

    fixed bug with skipping dialogue using spacebar

--- UnderTheCounterSource/Assets/Scripts/Technical/DebugLogger.cs
+++ UnderTheCounterSource/Assets/Scripts/Technical/DebugLogger.cs
@@ -5,7 +5,7 @@ namespace Technical
 {
     public class DebugLogger : MonoBehaviour
     {
-        uint qsize = 15;  // number of messages to keep
+        uint qsize = 10;  // number of messages to keep
         Queue myLogQueue = new Queue();
 
         void Start() {
