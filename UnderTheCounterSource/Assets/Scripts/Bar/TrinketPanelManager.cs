a7ff5c7a18ff55b678569acd642df96bcf874c60
Riccardo Speroni <riccardo.speroni@mail.polimi.it>
2025-01-19 15:32:31 +0100

    small fix

--- UnderTheCounterSource/Assets/Scripts/Bar/TrinketPanelManager.cs
+++ UnderTheCounterSource/Assets/Scripts/Bar/TrinketPanelManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using AYellowpaper.SerializedCollections.Editor.Data;
 using Newtonsoft.Json;
 using Technical;
 using TMPro;
